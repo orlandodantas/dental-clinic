0c91314cc4653e5ae4069414077035ec605d9516
Orlando Dantas <orlandodantas.1@hotmail.com>
2022-09-01 18:45:48 -0300

    feat: Implementing the class ServiceModel

---
+++ src/models/ServiceModel.ts
@@ -0,0 +1,43 @@
+import { PrismaClient, Service } from '@prisma/client';
+import { IServiceModel } from './interfaces';
+import { ServiceDTO } from './types';
+
+export default class ServiceModel implements IServiceModel {
+  private _connection: PrismaClient;
+
+  constructor(connection: PrismaClient) {
+    this._connection = connection;
+  }
+
+  public async getAll(skip: number, limit: number): Promise<ServiceDTO[]> {
+    return this._connection.service.findMany({
+      skip,
+      take: limit,
+    });
+  }
+
+  public async getByID(id: string): Promise<ServiceDTO> {
+    return this._connection.service.findUniqueOrThrow({
+      where: { id },
+    });
+  }
+
+  public async create(service: ServiceDTO): Promise<ServiceDTO> {
+    return this._connection.service.create({
+      data: service as Service,
+    });
+  }
+
+  public async update(id: string, service: ServiceDTO): Promise<ServiceDTO> {
+    return this._connection.service.update({
+      where: { id },
+      data: service,
+    });
+  }
+
+  public async delete(id: string): Promise<void> {
+    this._connection.service.delete({
+      where: { id },
+    });
+  }
+}
