32069ae6e3e71d300edf208dc0443c5960c4f243
Orlando Dantas <orlandodantas.1@hotmail.com>
2022-09-01 19:26:39 -0300

    feat: Implementing the interface ISaleModel

---
+++ src/models/interfaces/ISaleModel.ts
@@ -0,0 +1,9 @@
+import { SaleDTO } from '../types';
+
+export default interface ISaleModel {
+  getAll(skip: number, limit: number): Promise<SaleDTO[]>;
+  getByID(id: string): Promise<SaleDTO>;
+  create(sale: SaleDTO): Promise<SaleDTO>;
+  update(id: string, sale: SaleDTO): Promise<SaleDTO>;
+  delete(id: string): Promise<void>;
+}
