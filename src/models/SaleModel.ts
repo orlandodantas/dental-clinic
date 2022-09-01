7fe4ea7b217dab516fcfbcee28e49c7d5d2029c9
Orlando Dantas <orlandodantas.1@hotmail.com>
2022-09-01 19:27:31 -0300

    feat: Implementing the class SaleModel

---
+++ src/models/SaleModel.ts
@@ -0,0 +1,43 @@
+import { PrismaClient, Sale } from '@prisma/client';
+import { ISaleModel } from './interfaces';
+import { SaleDTO } from './types';
+
+export default class SaleModel implements ISaleModel {
+  private _connection: PrismaClient;
+
+  constructor(connection: PrismaClient) {
+    this._connection = connection;
+  }
+
+  public async getAll(skip: number, limit: number): Promise<SaleDTO[]> {
+    return this._connection.sale.findMany({
+      skip,
+      take: limit,
+    });
+  }
+
+  public async getByID(id: string): Promise<SaleDTO> {
+    return this._connection.sale.findUniqueOrThrow({
+      where: { id },
+    });
+  }
+
+  public async create(sale: SaleDTO): Promise<SaleDTO> {
+    return this._connection.sale.create({
+      data: sale as Sale,
+    });
+  }
+
+  public async update(id: string, sale: SaleDTO): Promise<SaleDTO> {
+    return this._connection.sale.update({
+      where: { id },
+      data: sale as Sale,
+    });
+  }
+
+  public async delete(id: string): Promise<void> {
+    this._connection.sale.delete({
+      where: { id },
+    });
+  }
+}
