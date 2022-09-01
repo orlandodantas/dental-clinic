bfed60854402816dfa498c44d6468369872e9029
Orlando Dantas <orlandodantas.1@hotmail.com>
2022-09-01 17:20:25 -0300

    feat: Creating the interface IUserModel

---
+++ src/models/interfaces/IUser.ts
@@ -0,0 +1,9 @@
+import { UserDTO } from '../types';
+
+export default interface IUserModel {
+  getAll(skip: number, limit: number): Promise<UserDTO[]>;
+  getByID(id: string): Promise<UserDTO>;
+  create(user: UserDTO): Promise<UserDTO>;
+  update(id: string, user: UserDTO): Promise<UserDTO>;
+  delete(id: string): Promise<void>;
+}
