abf6136e481c97eb8b4030ef06c374442db6f980
Orlando Dantas <orlandodantas.1@hotmail.com>
2022-09-01 18:46:58 -0300

    chore: Organizing Imports

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,5 +1,6 @@
 import connection from './connection';
 import PatientModel from './PatientModel';
+import ServiceModel from './ServiceModel';
 import UserModel from './UserModel';
 
-export { connection, UserModel, PatientModel };
+export { connection, UserModel, PatientModel, ServiceModel };
