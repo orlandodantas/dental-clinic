0f5575cae269989d1ca8fb060976a858282e08ad
Orlando Dantas <orlandodantas.1@hotmail.com>
2022-09-01 18:40:55 -0300

    chore: Organizing Imports

--- src/models/types/index.ts
+++ src/models/types/index.ts
@@ -1,4 +1,5 @@
 import PatientDTO from './PatientDTO';
+import ServiceDTO from './ServiceDTO';
 import { Role, UserDTO } from './UserDTO';
 
-export { UserDTO, Role, PatientDTO };
+export { UserDTO, Role, PatientDTO, ServiceDTO };
