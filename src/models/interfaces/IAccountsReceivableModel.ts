77c81b7883ed33e217e6b34ff608035ef4a99e24
Orlando Dantas <orlandodantas.1@hotmail.com>
2022-09-01 21:20:51 -0300

    feat: Implementing the interface IAccountsReceivableModel

---
+++ src/models/interfaces/IAccountsReceivableModel.ts
@@ -0,0 +1,9 @@
+import { AccountsReceivableDTO } from '../types';
+
+export default interface IAccountsReceivableModel {
+  getAll(skip: number, limit: number): Promise<AccountsReceivableDTO[]>;
+  getByID(id: string): Promise<AccountsReceivableDTO>;
+  create(AccountsReceivableDTO: AccountsReceivableDTO): Promise<AccountsReceivableDTO>;
+  update(id: string, AccountsReceivableDTO: AccountsReceivableDTO): Promise<AccountsReceivableDTO>;
+  delete(id: string): Promise<void>;
+}
