61b545f3a2c5f0aa5ac1cf2a2df492a195eabb7c
Orlando Dantas <orlandodantas.1@hotmail.com>
2022-09-01 21:21:17 -0300

    chore: Organizing Imports

--- src/models/interfaces/index.ts
+++ src/models/interfaces/index.ts
@@ -1,8 +1,16 @@
 /* eslint-disable import/prefer-default-export */
+import IAccountsReceivableModel from './IAccountsReceivableModel';
 import IPatientModel from './IPatientModel';
 import ISaleItemModel from './ISaleItemModel';
 import ISaleModel from './ISaleModel';
 import IServiceModel from './IServiceModel';
 import IUserModel from './IUserModel';
 
-export { IUserModel, IPatientModel, IServiceModel, ISaleModel, ISaleItemModel };
+export {
+  IUserModel,
+  IPatientModel,
+  IServiceModel,
+  ISaleModel,
+  ISaleItemModel,
+  IAccountsReceivableModel,
+};
