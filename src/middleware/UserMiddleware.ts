5a388d6d34dcbc7ea18d73e71394988418863412
Orlando Dantas <orlandodantas.1@hotmail.com>
2022-09-04 15:35:33 -0300

    feat: Adding the VerifyLogin method to the UserMiddleware class

--- src/middleware/UserMiddleware.ts
+++ src/middleware/UserMiddleware.ts
@@ -37,7 +37,7 @@ export default class UserMiddleware {
     const { error } = schema.validate(req.body);
 
     if (error) {
-      console.log(error);
+      console.error(error);
     }
 
     if (error) return res.status(StatusCodes.BAD_REQUEST).json({ message: error.message });
@@ -71,7 +71,34 @@ export default class UserMiddleware {
     const { error } = schema.validate(req.body);
 
     if (error) {
-      console.log(error);
+      console.error(error);
+    }
+
+    if (error) return res.status(StatusCodes.BAD_REQUEST).json({ message: error.message });
+
+    next();
+  }
+
+  public static verifyLogin(req: Request, res: Response, next: NextFunction): Response | void {
+    const schema = Joi.object({
+      email: Joi.string().email().required().messages({
+        'string.base': '"email" deve ser um texto',
+        'string.empty': '"email" não pode ser vazia',
+        'string.email': '"email" precisa conter um e-mail válido',
+        'any.required': '"email" é obrigatório',
+      }),
+      password: Joi.string().min(6).required().messages({
+        'string.base': '"password" deve ser um texto',
+        'string.min': '"password" deve ser no mínimo 6 caracteres',
+        'string.empty': '"password" não pode ser vazia',
+        'any.required': '"password" é obrigatório',
+      }),
+    });
+
+    const { error } = schema.validate(req.body);
+
+    if (error) {
+      console.error(error);
     }
 
     if (error) return res.status(StatusCodes.BAD_REQUEST).json({ message: error.message });
