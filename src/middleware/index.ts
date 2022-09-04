88cebfe662f94745dc699ab0cb29bf3d777e1699
Orlando Dantas <orlandodantas.1@hotmail.com>
2022-09-04 19:09:50 -0300

    chore: Organizing Imports

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -1,6 +1,7 @@
 import Authorization from './Authorization';
 import CatchError from './CatchError';
 import Pagination from './Pagination';
+import PatientValidate from './PatientValidate';
 import UserValidate from './UserValidate';
 
-export { CatchError, Pagination, UserValidate, Authorization };
+export { CatchError, Pagination, UserValidate, Authorization, PatientValidate };
