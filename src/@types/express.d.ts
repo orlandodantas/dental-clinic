6bba063fa4956c5b976973c935dbe1ea8f9558c2
Orlando Dantas <orlandodantas.1@hotmail.com>
2022-09-04 16:20:56 -0300

    feat: Implementing the type login to express Request

--- src/@types/express.d.ts
+++ src/@types/express.d.ts
@@ -1,10 +1,17 @@
 declare namespace Express {
   export interface Request {
     pagination: Pagination;
+    login: Login;
   }
 }
 
 type Pagination = {
   skip: number;
   limit: number;
+};
+
+type Login = {
+  name: string;
+  email: string;
+  role: Role;
 };
