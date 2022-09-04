ed46237372d17b4bc0502e3582e0191f65957b3c
Orlando Dantas <orlandodantas.1@hotmail.com>
2022-09-04 10:14:29 -0300

    feat: Adjusting for UserModel methods not returning the password field.

--- src/models/UserModel.ts
+++ src/models/UserModel.ts
@@ -4,27 +4,55 @@ import { IUserModel } from './interfaces';
 
 export default class UserModel implements IUserModel {
   private _connection: PrismaClient;
+  private _selectFields: {
+    id: boolean;
+    name: boolean;
+    email: boolean;
+    role: boolean;
+    createdAt: boolean;
+    updatedAt: boolean;
+    password: boolean;
+  };
 
   constructor(connection: PrismaClient) {
     this._connection = connection;
+    this._selectFields = {
+      id: true,
+      name: true,
+      email: true,
+      role: true,
+      createdAt: true,
+      updatedAt: true,
+      password: false,
+    };
   }
 
   public async getAll(skip: number, limit: number): Promise<UserDTO[]> {
     return this._connection.user.findMany({
       skip,
       take: limit,
+      select: this._selectFields,
     });
   }
 
-  public async getByID(id: string): Promise<UserDTO> {
-    return this._connection.user.findUniqueOrThrow({
+  public async getByID(id: string): Promise<UserDTO | null> {
+    return this._connection.user.findUnique({
       where: { id },
+      select: this._selectFields,
+    });
+  }
+
+  public async getByEmail(email: string): Promise<UserDTO | null> {
+    return this._connection.user.findUnique({
+      where: { email },
+      select: this._selectFields,
     });
   }
 
   public async create(user: UserDTO): Promise<UserDTO> {
     return this._connection.user.create({
       data: user as User,
+      select: this._selectFields,
     });
   }
 
@@ -32,11 +60,12 @@ export default class UserModel implements IUserModel {
     return this._connection.user.update({
       where: { id },
       data: user,
+      select: this._selectFields,
     });
   }
 
   public async delete(id: string): Promise<void> {
-    this._connection.user.delete({
+    await this._connection.user.delete({
       where: { id },
     });
   }
