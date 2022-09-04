1d0870cba263b3d3b2e6cc470dc8ef46abc2245f
Orlando Dantas <orlandodantas.1@hotmail.com>
2022-09-04 15:32:22 -0300

    feat: Adding login method to UserController class

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,3 +1,4 @@
+import { Request, Response } from 'express';
 import { IUserService } from '../services/interfaces';
 import { UserDTO } from '../types';
 import GenericController from './GenericController';
@@ -9,5 +10,15 @@ export default class UserController extends GenericController<UserDTO> implement
   constructor(service: IUserService) {
     super(service);
     this._userService = service;
+
+    this.login = this.login.bind(this);
+  }
+
+  public async login(req: Request, res: Response): Promise<void> {
+    const { email, password } = req.body;
+
+    const token = await this._userService.login(email, password);
+
+    res.status(200).json({ token });
   }
 }
