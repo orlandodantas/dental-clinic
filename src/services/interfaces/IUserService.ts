9fde54b2fe15f2ea6e225c1c6470f0723377c19b
Orlando Dantas <orlandodantas.1@hotmail.com>
2022-09-04 15:27:37 -0300

    feat: Adding the login method to the IUserService interface

--- src/services/interfaces/IUserService.ts
+++ src/services/interfaces/IUserService.ts
@@ -1,4 +1,6 @@
 import { UserDTO } from '../../types';
 import IGenericService from './IGenericService';
 
-export default interface IUserService extends IGenericService<UserDTO> {}
+export default interface IUserService extends IGenericService<UserDTO> {
+  login(email: string, password: string): Promise<string>;
+}
