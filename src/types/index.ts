4b51fe9ec76f33162f8e830ef2e498926c931156
Orlando Dantas <orlandodantas.1@hotmail.com>
2022-09-04 15:30:50 -0300

    chore: Organizing Imports

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,8 +1,18 @@
 import AccountsReceivableDTO from './AccountsReceivableDTO';
 import PatientDTO from './PatientDTO';
+import Payload from './Payload';
 import SaleDTO from './SaleDTO';
 import SaleItemDTO from './SaleItemDTO';
 import ServiceDTO from './ServiceDTO';
 import UserDTO, { Role } from './UserDTO';
 
-export { UserDTO, Role, PatientDTO, ServiceDTO, SaleDTO, SaleItemDTO, AccountsReceivableDTO };
+export {
+  UserDTO,
+  Role,
+  PatientDTO,
+  ServiceDTO,
+  SaleDTO,
+  SaleItemDTO,
+  AccountsReceivableDTO,
+  Payload,
+};
