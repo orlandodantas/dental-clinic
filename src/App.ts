bcf3bb9d3f3551d4809e4d97e42a79ae10d186fc
Orlando Dantas <orlandodantas.1@hotmail.com>
2022-09-05 17:34:13 -0300

    feat: Adding endpoint /services

--- src/App.ts
+++ src/App.ts
@@ -1,7 +1,7 @@
 import cors from 'cors';
 import express from 'express';
 import { CatchError } from './middleware';
-import { LoginRouter, PatientRouter, UserRouter } from './routers';
+import { LoginRouter, PatientRouter, ServiceRouter, UserRouter } from './routers';
 
 export default class App {
   public app: express.Express;
@@ -19,7 +19,8 @@ export default class App {
     this.app
       .use('/users', new UserRouter().route)
       .use('/login', new LoginRouter().route)
-      .use('/patients', new PatientRouter().route);
+      .use('/patients', new PatientRouter().route)
+      .use('/services', new ServiceRouter().route);
 
     // Middleware de Error
     this.app.use(CatchError.error);
