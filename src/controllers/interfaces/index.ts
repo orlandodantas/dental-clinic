10dc14e833a5381664cba43db0ac0449cfe3a4a6
Orlando Dantas <orlandodantas.1@hotmail.com>
2022-09-10 11:04:29 -0300

    chore: Organizing imports

--- src/controllers/interfaces/index.ts
+++ src/controllers/interfaces/index.ts
@@ -1,6 +1,13 @@
 import IGenericController from './IGenericController';
 import IPatientController from './IPatientController';
+import ISaleController from './ISaleController';
 import IServiceController from './IServiceController';
 import IUserController from './IUserController';
 
-export { IUserController, IGenericController, IPatientController, IServiceController };
+export {
+  IUserController,
+  IGenericController,
+  IPatientController,
+  IServiceController,
+  ISaleController,
+};
