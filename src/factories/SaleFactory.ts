c33e1ffd5722dc1df0c25ea0a5b7e16e7915ef56
Orlando Dantas <orlandodantas.1@hotmail.com>
2022-09-10 11:25:11 -0300

    feat: Creating the class SaleFactory

---
+++ src/factories/SaleFactory.ts
@@ -0,0 +1,14 @@
+import { SaleController } from '../controllers';
+import { ISaleController } from '../controllers/interfaces';
+import { connection, SaleModel } from '../models';
+import { SaleService } from '../services';
+
+export default class SaleFactory {
+  public static create(): ISaleController {
+    const saleModel = new SaleModel(connection);
+    const saleService = new SaleService(saleModel);
+    const saleController = new SaleController(saleService);
+
+    return saleController;
+  }
+}
