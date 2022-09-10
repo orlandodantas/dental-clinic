634b867aaa3b81c2ec1fdb51ab2915cec30e0015
Orlando Dantas <orlandodantas.1@hotmail.com>
2022-09-10 11:43:28 -0300

    chore: Organizing imports

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -1,6 +1,7 @@
 import LoginRouter from './LoginRouter';
 import PatientRouter from './PatientRouter';
+import SaleRouter from './SaleRouter';
 import ServiceRouter from './ServiceRouter';
 import UserRouter from './UserRouter';
 
-export { UserRouter, LoginRouter, PatientRouter, ServiceRouter };
+export { UserRouter, LoginRouter, PatientRouter, ServiceRouter, SaleRouter };
