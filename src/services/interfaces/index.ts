e69959cec4a1a84edc8d6538fd6bf1bca0690ce2
Orlando Dantas <orlandodantas.1@hotmail.com>
2022-09-10 10:56:35 -0300

    chore: Organizing imports

--- src/services/interfaces/index.ts
+++ src/services/interfaces/index.ts
@@ -1,6 +1,7 @@
 import IGenericService from './IGenericService';
 import IPatientService from './IPatientService';
+import ISaleService from './ISaleService';
 import IServiceService from './IServiceService';
 import IUserService from './IUserService';
 
-export { IUserService, IGenericService, IPatientService, IServiceService };
+export { IUserService, IGenericService, IPatientService, IServiceService, ISaleService };
