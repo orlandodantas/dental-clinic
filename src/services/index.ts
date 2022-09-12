0d7e131987b6a08b3560857920ee13d88f098d2a
Orlando Dantas <orlandodantas.1@hotmail.com>
2022-09-12 11:48:15 -0300

    chore: Organizing imports

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,7 +1,15 @@
+import AccountsReceivableService from './AccountsReceivableService';
 import GenericService from './GenericService';
 import PatientService from './PatientService';
 import SaleService from './SaleService';
 import ServiceService from './ServiceService';
 import UserService from './UserService';
 
-export { UserService, GenericService, PatientService, ServiceService, SaleService };
+export {
+  UserService,
+  GenericService,
+  PatientService,
+  ServiceService,
+  SaleService,
+  AccountsReceivableService,
+};
