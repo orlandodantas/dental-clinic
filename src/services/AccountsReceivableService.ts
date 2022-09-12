0d02826fb9d6165cb1e6fbd9539cf4ba104d259c
Orlando Dantas <orlandodantas.1@hotmail.com>
2022-09-12 11:47:53 -0300

    feat: Creating the class AccountsReceivableService

---
+++ src/services/AccountsReceivableService.ts
@@ -0,0 +1,25 @@
+import { ConflictError } from 'restify-errors';
+import { IAccountsReceivableModel } from '../models/interfaces';
+import { AccountsReceivableDTO } from '../types';
+import GenericService from './GenericService';
+import { IAccountsReceivableService } from './interfaces';
+
+export default class AccountsReceivableService
+  extends GenericService<AccountsReceivableDTO>
+  implements IAccountsReceivableService
+{
+  private _accountsReceivableModel: IAccountsReceivableModel;
+
+  constructor(model: IAccountsReceivableModel) {
+    super(model);
+    this._accountsReceivableModel = model;
+  }
+
+  public async patchReceive(id: string, date: Date): Promise<AccountsReceivableDTO> {
+    const receiveData = await super.getByID(id);
+
+    if (receiveData.receivingDate) throw new ConflictError('Conta já foi recebida!');
+
+    return this._accountsReceivableModel.patchReceive(id, date);
+  }
+}
