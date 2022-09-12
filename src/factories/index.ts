4cbb2d946d83925a4034ff9a5462c5cddb90a2cb
Orlando Dantas <orlandodantas.1@hotmail.com>
2022-09-12 11:52:04 -0300

    chore: Organizing imports

--- src/factories/index.ts
+++ src/factories/index.ts
@@ -1,6 +1,7 @@
+import AccountsReceivableFactory from './AccountsReceivableFactory';
 import PatientFactory from './PatientFactory';
 import SaleFactory from './SaleFactory';
 import ServiceFactory from './ServiceFactory';
 import UserFactory from './UserFactory';
 
-export { UserFactory, PatientFactory, ServiceFactory, SaleFactory };
+export { UserFactory, PatientFactory, ServiceFactory, SaleFactory, AccountsReceivableFactory };
