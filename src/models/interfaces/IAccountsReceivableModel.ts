19684cd2d3e79827f320706c4b03c787345dace6
Orlando Dantas <orlandodantas.1@hotmail.com>
2022-09-13 00:36:31 -0300

    feat: Adding methods the interface IAccountsReceivableModel

--- src/models/interfaces/IAccountsReceivableModel.ts
+++ src/models/interfaces/IAccountsReceivableModel.ts
@@ -3,4 +3,6 @@ import IGenericModel from './IGenericModel';
 
 export default interface IAccountsReceivableModel extends IGenericModel<AccountsReceivableDTO> {
   patchReceive(id: string, date: Date): Promise<AccountsReceivableDTO>;
+  getByReceived(dateInitial: Date, dateEnd: Date): Promise<AccountsReceivableDTO[]>;
+  getByReceive(dateInitial: Date, dateEnd: Date): Promise<AccountsReceivableDTO[]>;
 }
