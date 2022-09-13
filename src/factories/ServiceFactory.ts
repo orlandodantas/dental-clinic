032786dce60f932211dc0547056ebf46b91cc2f5
Orlando Dantas <orlandodantas.1@hotmail.com>
2022-09-12 23:08:37 -0300

    feat: Adding method Service for class ServiceFactory

--- src/factories/ServiceFactory.ts
+++ src/factories/ServiceFactory.ts
@@ -1,7 +1,9 @@
+/* eslint-disable import/no-cycle */
 import { ServiceController } from '../controllers';
 import { IServiceController } from '../controllers/interfaces';
 import { connection, ServiceModel } from '../models';
 import { ServiceService } from '../services';
+import { IServiceService } from '../services/interfaces';
 
 export default class ServiceFactory {
   public static create(): IServiceController {
@@ -10,5 +12,12 @@ export default class ServiceFactory {
     const serviceController = new ServiceController(serviceService);
 
     return serviceController;
+  }
+
+  public static Service(): IServiceService {
+    const serviceModel = new ServiceModel(connection);
+    const service = new ServiceService(serviceModel);
+
+    return service;
   }
 }
