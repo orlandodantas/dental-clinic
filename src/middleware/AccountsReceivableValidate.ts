5218bbad00f8002635ebaaa91cedeb07312f2524
Orlando Dantas <orlandodantas.1@hotmail.com>
2022-09-13 00:39:04 -0300

    feat: Creating method verifyReceivables for middleware AccountsReceivableValidate

--- src/middleware/AccountsReceivableValidate.ts
+++ src/middleware/AccountsReceivableValidate.ts
@@ -23,4 +23,34 @@ export default class AccountsReceivableValidate {
 
     next();
   }
+
+  public static verifyReceivables(
+    req: Request,
+    res: Response,
+    next: NextFunction,
+  ): Response | void {
+    const schema = Joi.object({
+      dateInitial: Joi.date().iso().required().messages({
+        'date.base': '"dateInitial" deve ser um data no formato 2022-09-12T18:33:42.775Z',
+        'date.empty': '"dateInitial" não pode ser vazio',
+        'any.required': '"dateInitial" é obrigatório',
+      }),
+      dateEnd: Joi.date().iso().required().messages({
+        'date.base': '"dateEnd" deve ser um data no formato 2022-09-12T18:33:42.775Z',
+        'date.empty': '"dateEnd" não pode ser vazio',
+        'any.required': '"dateEnd" é obrigatório',
+      }),
+    });
+
+    const { error } = schema.validate(req.body);
+
+    if (error) {
+      return res.status(StatusCodes.BAD_REQUEST).json({ message: error.message });
+    }
+
+    req.body.dateInitial = new Date(req.body.dateInitial).toISOString();
+    req.body.dateEnd = new Date(req.body.dateEnd).toISOString();
+
+    next();
+  }
 }
