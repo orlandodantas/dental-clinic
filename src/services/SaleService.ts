ddb9af31212dfcf491b04fd69226dafe60908493
Orlando Dantas <orlandodantas.1@hotmail.com>
2022-09-12 23:05:49 -0300

    feat: Doing some checks and validations for the create and update of saleService

--- src/services/SaleService.ts
+++ src/services/SaleService.ts
@@ -1,11 +1,15 @@
-import { BadRequestError } from 'restify-errors';
+import { BadRequestError, InternalServerError } from 'restify-errors';
 import { ISaleModel } from '../models/interfaces';
-import { SaleDTO } from '../types';
+import { SaleDTO, SaleItemDTO } from '../types';
 import GenericService from './GenericService';
-import { ISaleService } from './interfaces';
+import { IPatientService, ISaleService, IServiceService } from './interfaces';
 
 export default class SaleService extends GenericService<SaleDTO> implements ISaleService {
   private _saleModel: ISaleModel;
+  private _patient!: IPatientService;
+  private _service!: IServiceService;
+  private _messageServerError =
+    'Houve um problema em nosso servidor, estamos trabalhando para resolver o mais rápido possível.';
 
   constructor(model: ISaleModel) {
     super(model);
@@ -16,12 +20,18 @@ export default class SaleService extends GenericService<SaleDTO> implements ISal
     SaleService._verifySaleItem(entity);
     SaleService._verifyAccountsReceivable(entity);
 
+    await this._verifyPatient(entity.patientId);
+    await this._verifyService(entity.saleItem as SaleItemDTO[]);
+
     return super.create(entity);
   }
 
   public override async update(id: string, entity: SaleDTO): Promise<SaleDTO> {
     SaleService._verifySaleItem(entity);
     SaleService._verifyAccountsReceivable(entity);
+
+    await this._verifyPatient(entity.patientId);
+    await this._verifyService(entity.saleItem as SaleItemDTO[]);
 
     return super.update(id, entity);
   }
@@ -49,5 +59,39 @@ export default class SaleService extends GenericService<SaleDTO> implements ISal
       throw new BadRequestError(
         'O totalValue da venda precisa ser igual a soma de todos os value de accountReceivable!',
       );
+  }
+
+  private async _verifyPatient(patientId: string): Promise<void> {
+    if (!this._patient) {
+      throw new InternalServerError(this._messageServerError);
+    }
+
+    await this._patient.getByID(patientId);
+  }
+
+  private async _verifyService(saleItem: SaleItemDTO[]): Promise<void> {
+    if (!this._service) {
+      throw new InternalServerError(this._messageServerError);
+    }
+
+    await Promise.all(
+      saleItem.map(async ({ value, serviceId }) => {
+        const saleItemData = await this._service.getByID(serviceId);
+
+        if (saleItemData.value !== value) {
+          throw new BadRequestError(
+            `Valor informado em saleItem não confere com o valor do serviço de  ID: ${saleItemData.id}`,
+          );
+        }
+      }),
+    );
+  }
+
+  public set patient(patient: IPatientService) {
+    this._patient = patient;
+  }
+
+  public set service(service: IServiceService) {
+    this._service = service;
   }
 }
