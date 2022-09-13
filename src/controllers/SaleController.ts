30ee74acd8035d4d6d08bb5cba6158035801b998
Orlando Dantas <orlandodantas.1@hotmail.com>
2022-09-12 23:10:02 -0300

    feat: Tuning the SaleController to correctly serve the SaleService

--- src/controllers/SaleController.ts
+++ src/controllers/SaleController.ts
@@ -1,4 +1,7 @@
+/* eslint-disable import/no-cycle */
 import { Request, Response } from 'express';
+import { StatusCodes } from 'http-status-codes';
+import { PatientFactory, ServiceFactory } from '../factories';
 import { ISaleService } from '../services/interfaces';
 import { SaleDTO } from '../types';
 import GenericController from './GenericController';
@@ -16,13 +19,20 @@ export default class SaleController extends GenericController<SaleDTO> implement
     const { id } = req.login;
     req.body.userId = id;
 
-    await super.create(req, res);
+    this._saleService.patient = PatientFactory.PatientService();
+    this._saleService.service = ServiceFactory.Service();
+    const entity = await this._saleService.create(req.body);
+
+    res.status(StatusCodes.CREATED).json(entity);
   }
 
-  // public override async update(req: Request, res: Response): Promise<void> {
-  //   const { id } = req.login;
-  //   req.body.userId = id;
+  public override async update(req: Request, res: Response): Promise<void> {
+    const { id } = req.params;
 
-  //   await super.update(req, res);
-  // }
+    this._saleService.patient = PatientFactory.PatientService();
+    this._saleService.service = ServiceFactory.Service();
+    const entity = await this._saleService.update(id, req.body);
+
+    res.status(StatusCodes.OK).json(entity);
+  }
 }
